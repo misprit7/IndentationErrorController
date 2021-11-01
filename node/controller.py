e776b2f82ed013043ca1b21358107142bcb879de
Xander Naumenko <xandernaumenko@gmail.com>
2021-11-01 12:16:34 -0700

    Added pid controller

--- node/controller.py
+++ node/controller.py
@@ -1,22 +1,69 @@
 #! /usr/bin/env python
 
 import rospy
-from geometry_msgs.msg import Twist
 
+from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Image
 
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+
+bridge = CvBridge()
+
+# Init node
 rospy.init_node('indentation_error_controller')
 rate = rospy.Rate(2)
-move = Twist()
-move.linear.x = 0.1
-move.angular.z = 0.5
-pub = rospy.Publisher('/R1/cmd_vel', Twist, 
+
+
+move_pub = rospy.Publisher('/R1/cmd_vel', Twist, 
   queue_size=1)
 
-def image_callback(img_msg):
-    pub.publish(move)
+def show_image(img):
+    cv2.imshow("Image Window", img)
+    cv2.waitKey(3)
 
-sub = rospy.Subscriber("/R1/pi_camera/image_raw",Image,image_callback)
+def image_callback(img_msg):
+    try:
+        cv_image = bridge.imgmsg_to_cv2(img_msg, "passthrough")
+    except CvBridgeError, e:
+        rospy.logerr("CvBridge Error: {0}".format(e))
+
+
+    hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
+    frame_threshold = cv2.inRange(hsv, (0, 0, 80), (10, 10, 90))
+
+    height, width = frame_threshold.shape
+
+    M = cv2.moments(frame_threshold)
+    if M["m00"] == 0:
+      cX = width / 2
+      cY = height / 2
+    else:
+      cX = int(M["m10"] / M["m00"])
+      cY = int(M["m01"] / M["m00"])
+
+    cv2.circle(cv_image, (cX, cY), 5, [255, 255, 255], -1)
+
+    
+    wP = 1
+    wI = 1
+    wD = 1
+
+    p = -(2.0 * cX / width - 1) * wP
+
+    pid = p
+
+    move = Twist()
+    move.angular.z = p
+    move.linear.x = 0.2
+
+    move_pub.publish(move)
+
+    show_image(cv_image)
+
+image_sub = rospy.Subscriber("/R1/pi_camera/image_raw",Image,image_callback)
+velocity_pub = rospy.Publisher('/R1/cmd_vel', Twist, 
+  queue_size=1)
 
 
 while not rospy.is_shutdown():
---
+++ run_sim.sh
@@ -0,0 +1,2 @@
+source ~/ros_ws/devel/setup.bash
+~/ros_ws/src/2020_competition/enph353/enph353_utils/scripts/run_sim.sh -pgv

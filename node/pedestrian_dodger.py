59b6be04938a2969c24c4538b49068c4b04d9c86
Xander Naumenko <33139843+misprit7@users.noreply.github.com>
2021-12-05 18:28:36 -0800

    Update pedestrian_dodger.py

--- node/pedestrian_dodger.py
+++ node/pedestrian_dodger.py
@@ -11,10 +11,8 @@ def get_bottom_red(hsv, height):
     return bottom_red
 
 bg_subtract = cv2.bgsegm.createBackgroundSubtractorMOG()
-is_on_left = None
 last_center_x = 0
 def is_movement(img):
-    global is_on_left
     # Effectively a static variable
     global bg_subtract
     global last_center_x
@@ -25,13 +23,15 @@ def is_movement(img):
 
     count = np.count_nonzero(mask)
     print(count)
+    # If too much or too little on screen return
     if count > 1700 or count < 400:
         return True
     
+    # Get centroids
     m = cv2.moments(mask)
     if m['m00'] == 0:
         return True
-
+    # Check if change from left to right
     center_x = m['m10'] / m['m00']
     print(center_x, last_center_x, np.abs(center_x - last_center_x) < 60, max(center_x, last_center_x) > img.shape[1] / 2, min(center_x, last_center_x) < img.shape[1] / 2)
     if np.abs(center_x - last_center_x) < 50 and max(center_x, last_center_x) > img.shape[1] / 2 and min(center_x, last_center_x) < img.shape[1] / 2:
@@ -39,27 +39,3 @@ def is_movement(img):
     else:
         last_center_x = center_x
         return True
-
-    # print(is_on_left, m['m10'] / m['m00'])
-    # if is_on_left is None:
-    #     if m['m10'] / m['m00'] < img.shape[1] / 2:
-    #         is_on_left = True
-    #     else:
-    #         is_on_left = False
-
-
-    # if m['m10'] / m['m00'] < img.shape[1] / 2:
-    #     if is_on_left == False:
-    #         return False
-    # else:
-    #     if is_on_left == True:
-    #         return False
-    
-    # return True
-
-    # kernel = np.ones((2, 2), np.uint8)
-    # mask_erosion = cv2.erode(mask, kernel, iterations=2)
-
-    # cv2.imshow('Background Subtraction', mask_erosion)
-    # return np.count_nonzero(mask_erosion) > 5
-
